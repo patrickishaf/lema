f2087d40add828a879bb3cb90369365692db1981
oscarnovember <ndukwe.peter@roqqu.com>
2025-02-14 05:49:43 +0100

    make mobile table horizontally scrollable

--- frontend/src/components/UsersTable.jsx
+++ frontend/src/components/UsersTable.jsx
@@ -41,38 +41,40 @@ export default function UsersTable() {
 
   return (
     <section className="users-table w-full flex flex-col items-end">
-      <main className="table-main w-full rounded-lg border border-solid mb-6">
-        <div className="w-full flex items-center">
-          <div className="cell header one text-xs">Full Name</div>
-          <div className="cell header two text-xs">Email Address</div>
-          <div className="cell header three text-xs">Address</div>
-        </div>
-        {
-          isLoading
-            ?
-              <Loader />
-            : 
-              error
+      <div className="table-box w-full flex mb-6">
+        <main className="table-main w-full rounded-lg border border-solid">
+          <div className="w-full flex items-center">
+            <div className="cell header one text-xs">Full Name</div>
+            <div className="cell header two text-xs">Email Address</div>
+            <div className="cell header three text-xs">Address</div>
+          </div>
+          {
+            isLoading
               ?
-                <div>{error.message}</div>
-              :
-              userList.map(({ id, name, email, address }) => (
-                <div key={uuid()} className="detail-row flex items-center border-b cursor-pointer" onClick={() => {
-                  openRoute(`${routeNames.posts}/${id}`);
-                }}>
-                  <div className="cell detail one">
-                    <p className="detail-txt user-name font-medium text-sm">{name}</p>
+                <Loader />
+              : 
+                error
+                ?
+                  <div>{error.message}</div>
+                :
+                userList.map(({ id, name, email, address }) => (
+                  <div key={uuid()} className="detail-row flex items-center border-b cursor-pointer" onClick={() => {
+                    openRoute(`${routeNames.posts}/${id}`);
+                  }}>
+                    <div className="cell detail one">
+                      <p className="detail-txt user-name font-medium text-sm">{name}</p>
+                    </div>
+                    <div className="cell detail two">
+                      <p className="detail-txt user-email text-sm">{email}</p>
+                    </div>
+                    <div className="cell detail three">
+                      <p className="detail-txt user-address text-sm">{address}</p>
+                    </div>
                   </div>
-                  <div className="cell detail two">
-                    <p className="detail-txt user-email text-sm">{email}</p>
-                  </div>
-                  <div className="cell detail three">
-                    <p className="detail-txt user-address text-sm">{address}</p>
-                  </div>
-                </div>
-              ))
-        }
-      </main>
+                ))
+          }
+        </main>
+      </div>
       <div className="pagination-container">
       {
         users && users.totalPages && <PaginationComponent
