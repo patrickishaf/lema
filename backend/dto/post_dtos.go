a41824884de18933b5dca4cb4ffcd175f1143322
oscarnovember <ndukwe.peter@roqqu.com>
2025-02-14 02:35:56 +0100

    add more validation to dto

--- backend/dto/post_dtos.go
+++ backend/dto/post_dtos.go
@@ -1,7 +1,7 @@
 package dtos
 
 type CreatePostReqBody struct {
-	Title  string `json:"title" validate:"required"`
-	Body   string `json:"body" validate:"required"`
-	UserID uint   `json:"user_id" validate:"required"`
+	Title  string `json:"title" validate:"required,min=3,max=255,endsnotwith=."`
+	Body   string `json:"body" validate:"required,min=10,max=10000"`
+	UserID uint   `json:"user_id" validate:"required,min=1"`
 }
