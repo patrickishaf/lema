2eda7185aa67713507025d967cef8e6a0660ba47
oscarnovember <ndukwe.peter@roqqu.com>
2025-02-15 20:25:51 +0100

    reduce table height

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -2,7 +2,7 @@ import httpClient from "@/utils/httpclient";
 
 const userService = {
   async getUsers(pageNumber: number = 0) {
-    const res = await httpClient.get(`/users?pageNumber=${pageNumber}`);
+    const res = await httpClient.get(`/users?pageNumber=${pageNumber}&pageSize=4`);
     return res.data;
   },
 
